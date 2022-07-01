8012dfe66d50035a20c7054d569141d84f3c3381
sarakrieger <sara.krieger2022@gmail.com>
2022-07-01 11:21:18 +0200

    Update comment

--- src/main/java/Maintest1.java
+++ src/main/java/Maintest1.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 public class Maintest1 {
-
+//update
     public static int add(int bananas, int apples) {
     return bananas + apples;
 
--- src/test/java/Maintest1Test.java
+++ src/test/java/Maintest1Test.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Maintest1Test {
-
+//update
     @Test
             void addition1(){
 
